c47588fc029817ca167739a807ee1140eafa7f89
Kharis <maulanakharis129@gmail.com>
2026-09-15 19:26:17 +0700

    Initial Commit

---
+++ Kino/KinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinoApp.swift
+//  Kino
+//
+//  Created by Kharis Destian Maulana on 15/09/26.
+//
+
+import SwiftUI
+
+@main
+struct KinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
